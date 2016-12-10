7d6ef1b2fe939b245f24e4996b7d16d0e0629762
Michael Weidner <michael.weidner@motorolasolutions.com>
2016-12-09 21:46:12 -0500

    add baseline database read

--- solar_monitor.py
+++ solar_monitor.py
@@ -17,26 +17,26 @@ class SolarSensors:
 	def __init__(self):
 		self.m_sensors = [];
 		self.m_sensorNames = [];
-			
+
 	def addSensor(self,name,sensor):
 		self.m_sensors.append(sensor);
 		self.m_sensorNames.append(name);
-		
+
 	def getData(self):
 		returnVal = {};
-		
+
 		returnVal["names"] = [];
 		returnVal["voltage"] = [];
 		returnVal["current"] = [];
-		
+
 		for index in xrange(len(self.m_sensors)):
 			returnVal["names"].append(self.m_sensorNames[index]);
-			
+
 			voltage = self.m_sensors[index].getBusVoltage_V()
 			current = self.m_sensors[index].getCurrent_mA()
 			returnVal["voltage"].append( voltage );
 			returnVal["current"].append( current );
-			
+
 		return returnVal;
 
 class Solar:
@@ -44,7 +44,7 @@ class Solar:
 		self.m_SolarSensors = sensors;
 		self.m_SolarDb = SolarDb(filenamePrefix);
 		self.m_Timestamper = timestamper;
-		
+
 	def gatherData(self):
 		data = self.m_SolarSensors.getData();
 		return data;
@@ -56,30 +56,30 @@ class Solar:
 		returnValue.append( "%5.0f mA             %5.0f mA             %5.0f mA             %5.0f mA" % (results["current"][0],results["current"][1],results["current"][2],results["current"][3]));
 		returnValue.append( "%5.0f mW             %5.0f mW             %5.0f mW             %5.0f mW" % (results["voltage"][0]*results["current"][0],results["voltage"][1]*results["current"][1],results["voltage"][2]*results["current"][2],results["voltage"][3]*results["current"][3]));
 		return returnValue;
-		
+
 	def printResults(self, results):
 		text = self.formatPrintData(results)
 		print;
 		for index in xrange(len(text)):
 			print(text[index]);
-			
+
 	def recordData(self,data):
 		self.m_SolarDb.addEntry(self.m_Timestamper.getDate(), self.m_Timestamper.getTime(), data );
 
 class TimestamperInterface:
 	def getDate(self):
 		pass;
-		
+
 	def getTime(self):
 		pass
-		
+
 class Timestamper(TimestamperInterface):
 	def getDate(self):
 		return (time.strftime("%Y_%m_%d"))
-		
+
 	def getTime(self):
 		return (time.strftime("%H:%M:%S"))
-		
+
 class SolarDb:
 	def __init__(self, filenamePrefix):
 		self.totalEnergy = 0.0;
@@ -96,23 +96,23 @@ class SolarDb:
 			emptyList = []
 			self.m_currents.append(emptyList);
 		self.m_filenamePrefix = filenamePrefix;
-			
-		
+
+
 	def addEntry(self, date, time, data):
-		
+
 		# if rollover, flush the old data to the file.
 		sampleWindow = int(time[3:5])/10
 		if ( self.m_prev_sampleWindow != sampleWindow ) and (self.m_date != "0000_00_00"): # the hour rolled over.
 			m_prev_sampleWindow = sampleWindow;
-			
-			self.m_filename = self.m_filenamePrefix+self.m_date+".txt"
-			
+
+			self.m_filename = self.m_filenamePrefix+self.m_date+".csv"
+
 			# create the file if necessary
 			if not os.path.exists(self.m_filename):
 				f = open(self.m_filename, 'w')
-				f.write("time,%s_voltage,%s_current,%s_voltage,%s_current,%s_voltage,%s_current,%s_voltage,%s_current\n" % (data["names"][0], data["names"][0], data["names"][1], data["names"][1], data["names"][2], data["names"][2], data["names"][3], data["names"][3]))
+				f.write("time,%s voltage,%s current,%s voltage,%s current,%s voltage,%s current,%s voltage,%s current\n" % (data["names"][0], data["names"][0], data["names"][1], data["names"][1], data["names"][2], data["names"][2], data["names"][3], data["names"][3]))
 				f.close();
-			
+
 			# append the current data
 			f = open(self.m_filename, 'a')
 			print("length=%d" % (len(self.m_voltages[0])))
@@ -125,8 +125,8 @@ class SolarDb:
 						f.write(",");
 				f.write("\n");
 			f.close()
-							
-			
+
+
 			# clear the cached data for the next hour
 			self.m_voltages = [];
 			self.m_currents = [];
@@ -136,30 +136,81 @@ class SolarDb:
 				self.m_voltages.append(emptyList);
 				emptyList = []
 				self.m_currents.append(emptyList);
-		
+
 		self.m_date = date;
 		self.m_prev_sampleWindow = sampleWindow
-		
+
 		self.m_times.append(time);
 		for index in xrange(len(data["voltage"])):
 			self.m_voltages[index].append(data["voltage"][index]);
 			self.m_currents[index].append(data["current"][index]);
 			self.m_sensorNames.append(data["names"][index] );
+
+	# retval = [0-3] = {} "name"    = [0-3] = string "panel", "load", etc."
+	#                     "voltage" = [0-n] = float Volts
+	#                     "current" = [0-n] = int  mAmps
+	#                     "time"    = [0-n] = string "hh:mm:ss"
+	#
+	def readDayLog(self,date):
+		returnVal = [];
 		
+		for index in xrange(4):
+			tempVal = {}  # put an empty dictionary for each array entry.
+			tempVal["name"] = []
+			tempVal["voltage"] = []
+			tempVal["current"] = []
+			tempVal["time"] = []
+			
+			returnVal.append(tempVal);
+		filename = "solarLog_"+date+".csv"
+
+		fileHandle = open(filename,"r");
+		rawLines = fileHandle.readlines();
+		firstLineFields = rawLines[0].split(",");
+		
+		returnVal[0]["name"] = firstLineFields[1][:-8];  # strip off " voltage" from the end for the base name.
+		returnVal[1]["name"] = firstLineFields[3][:-8];  # strip off " voltage" from the end for the base name.
+		returnVal[2]["name"] = firstLineFields[5][:-8];  # strip off " voltage" from the end for the base name.
+		returnVal[3]["name"] = firstLineFields[7][:-8];  # strip off " voltage" from the end for the base name.
+		
+		for index in xrange(1,len(rawLines)):
+			fields = rawLines[index].split(",");
+			returnVal[0]["voltage"].append(fields[1])
+			returnVal[1]["voltage"].append(fields[3])
+			returnVal[2]["voltage"].append(fields[5])
+			returnVal[3]["voltage"].append(fields[7])
+			
+			returnVal[0]["current"].append(fields[2])
+			returnVal[1]["current"].append(fields[4])
+			returnVal[2]["current"].append(fields[6])
+			returnVal[3]["current"].append(fields[8])
+			
+			returnVal[0]["time"].append(fields[0])
+			returnVal[1]["time"].append(fields[0])
+			returnVal[2]["time"].append(fields[0])
+			returnVal[3]["time"].append(fields[0])
+			
+			
+		
+		
+		
+		fileHandle.close()
+		return returnVal;
+
 def setupSolar():
 	mySolarSensors = SolarSensors()
 
-#	ina = INA219(0x40);
-#	mySolarSensors.addSensor("Panel", ina ); # no jumpers.
-#	mySolarSensors.addSensor("Battery1", ina ); # A0 jumper.
-#	mySolarSensors.addSensor("Battery2", ina ); # A1 jumper.
-#	mySolarSensors.addSensor("Load", ina ); # A0 and A1 jumpers.
+#   ina = INA219(0x40);
+#   mySolarSensors.addSensor("Panel", ina ); # no jumpers.
+#   mySolarSensors.addSensor("Battery1", ina ); # A0 jumper.
+#   mySolarSensors.addSensor("Battery2", ina ); # A1 jumper.
+#   mySolarSensors.addSensor("Load", ina ); # A0 and A1 jumpers.
 
 	mySolarSensors.addSensor("Solar Panel (45)", INA219(0x45) ); # A0 and A1 jumpers.
 	mySolarSensors.addSensor("Battery 1 (44)", INA219(0x44) ); # A1 jumper.
 	mySolarSensors.addSensor("Battery 2 (41)", INA219(0x41) ); # A0 jumper.
 	mySolarSensors.addSensor("Load (40)", INA219(0x40) ); # no jumpers.
-	
+
 	mySolar = Solar(mySolarSensors, Timestamper() );
 	return mySolar;
 
@@ -173,8 +224,8 @@ def main():
 		# update gui
 		time.sleep(1.0)
 
-	
-	
+
+
 
 if __name__ == "__main__":
 	main();
--- solar_monitor_test.py
+++ solar_monitor_test.py
@@ -14,10 +14,10 @@ class INA219_Dummy:
 		self.m_voltage = 12.0000;
 		self.m_current = 100.0;
 		self.m_address = address;
-		
+
 		self.m_voltageDelta = 0.1;
 		self.m_currentDelta = 100.0;
-		
+
 	def getBusVoltage_V(self):
 		self.m_voltage = self.m_voltage + self.m_voltageDelta
 		if (self.m_voltage > 20.0):
@@ -25,7 +25,7 @@ class INA219_Dummy:
 		if (self.m_voltage < 10.0):
 			self.m_voltageDelta = 0.2;
 		return self.m_voltage
-		
+
 	def getCurrent_mA(self):
 		self.m_current = self.m_current + self.m_currentDelta
 		if (self.m_current > 2000.0):
@@ -33,15 +33,15 @@ class INA219_Dummy:
 		if (self.m_current < -2000.0):
 			self.m_currentDelta = 100.0;
 		return self.m_current
-		
+
 	def getPower_mW(self):
 		return self.m_current*self.m_voltageDelta
-		
+
 	def getAddr(self):
 		return self.m_address;
-		
-	
-	
+
+
+
 class TimestamperMock(TimestamperInterface):
 	def __init__(self):
 		self.m_year = 2016
@@ -53,11 +53,11 @@ class TimestamperMock(TimestamperInterface):
 
 	def getDate(self):
 		return ("%02d_%02d_%02d" % (self.m_year, self.m_month, self.m_day) )
-		
+
 	def getTime(self):
-#		self.m_secs = self.m_secs +1;
+#       self.m_secs = self.m_secs +1;
 		return ("%02d:%02d:%02d" % (self.m_hours, self.m_mins, self.m_secs) )
-		
+
 	def setDateTime(self, year, month, day, hour, mins, secs):
 		self.m_year = year
 		self.m_month = month
@@ -65,7 +65,7 @@ class TimestamperMock(TimestamperInterface):
 		self.m_hours = hour
 		self.m_mins = mins
 		self.m_secs = secs
-		
+
 	def advanceOneSec(self):
 		self.m_secs = self.m_secs + 1
 		if ( self.m_secs == 60 ):
@@ -84,7 +84,7 @@ class TimestamperMock(TimestamperInterface):
 			self.m_month = 1
 			self.m_year = self.m_year + 1
 
-		
+
 
 
 
@@ -95,38 +95,38 @@ def getSensors():
 	m_SolarSensors.addSensor("Battery1",INA219_Dummy(0x41));
 	m_SolarSensors.addSensor("Load",INA219_Dummy(0x42));
 	m_SolarSensors.addSensor("Battery2",INA219_Dummy(0x43));
-	
+
 	m_SolarSensors.m_sensors[0].m_voltage = 12.0;
 	m_SolarSensors.m_sensors[1].m_voltage = 13.0;
 	m_SolarSensors.m_sensors[2].m_voltage = 14.0;
 	m_SolarSensors.m_sensors[3].m_voltage = 15.0;
-			
+
 	m_SolarSensors.m_sensors[0].m_current = 400.0;
 	m_SolarSensors.m_sensors[1].m_current = 300.0;
 	m_SolarSensors.m_sensors[2].m_current = 200.0;
 	m_SolarSensors.m_sensors[3].m_current = 100.0;
 	return m_SolarSensors;
-	
+
 class TestSolar(unittest.TestCase):
-	
+
 	def setUp(self):
 		self.m_SolarSensors = getSensors();
 		self.m_TimestamperMock = TimestamperMock()
 		self.m_solar = Solar( self.m_SolarSensors, self.m_TimestamperMock ,filenamePrefix="test_solarLog_")
 		self.purge();
-				
+
 	def tearDown(self):
 		self.m_SolarSensors = None;
 		self.m_TimestamperMock = None;
 		self.m_solar = None;
 		self.purge();
-		
+
 	def purge(self):
 		dir = "."
 		for f in os.listdir(dir):
-			if ( "test_solarLog_" == f[:14]) and (".txt" == f[-4:]):
+			if ( "test_solarLog_" == f[:14]) and (".csv" == f[-4:]):
 				os.remove(os.path.join(dir, f))
-	
+
 	def checkNames(self, results):
 		if not "names" in results:
 			self.fail();
@@ -138,28 +138,28 @@ class TestSolar(unittest.TestCase):
 		self.assertEqual(results["names"][1], "Battery1")
 		self.assertEqual(results["names"][2], "Load")
 		self.assertEqual(results["names"][3], "Battery2")
-		
+
 	def checkParameter(self, results, parameter, expected):
 		self.assertAlmostEqual(results[parameter][0], expected[0], places=7)
 		self.assertAlmostEqual(results[parameter][1], expected[1], places=7)
 		self.assertAlmostEqual(results[parameter][2], expected[2], places=7)
 		self.assertAlmostEqual(results[parameter][3], expected[3], places=7)
 
-	
+
 	def test_SolarSensors_ctor(self):
 		self.assertEqual(self.m_SolarSensors.m_sensors[0].getAddr(), 0x40)
 		self.assertEqual(self.m_SolarSensors.m_sensors[1].getAddr(), 0x41)
 		self.assertEqual(self.m_SolarSensors.m_sensors[2].getAddr(), 0x42)
 		self.assertEqual(self.m_SolarSensors.m_sensors[3].getAddr(), 0x43)
-		
+
 		self.assertEqual(self.m_SolarSensors.m_sensorNames[0], "Panel")
 		self.assertEqual(self.m_SolarSensors.m_sensorNames[1], "Battery1")
 		self.assertEqual(self.m_SolarSensors.m_sensorNames[2], "Load")
 		self.assertEqual(self.m_SolarSensors.m_sensorNames[3], "Battery2")
-		
+
 	def test_SolarSensors_getData_1(self):
 		results = self.m_SolarSensors.getData();
-		
+
 		self.checkNames(results);
 		self.checkParameter(results, "voltage", (12.1, 13.1, 14.1, 15.1) )
 		self.checkParameter(results, "current", (500.0, 400.0, 300.0, 200.0) )
@@ -168,7 +168,7 @@ class TestSolar(unittest.TestCase):
 	def test_SolarSensors_getData_2(self):
 		results = self.m_SolarSensors.getData();
 		results = self.m_SolarSensors.getData();
-		
+
 		self.checkNames(results);
 		self.checkParameter(results, "voltage", (12.2, 13.2, 14.2, 15.2) )
 		self.checkParameter(results, "current", (600.0, 500.0, 400.0, 300.0) )
@@ -185,8 +185,8 @@ class TestSolar(unittest.TestCase):
 		self.checkNames(results);
 		self.checkParameter(results, "voltage", (12.1, 13.1, 14.1, 15.1) )
 		self.checkParameter(results, "current", (500.0, 400.0, 300.0, 200.0) )
-		self.m_solar.printResults(results)
-		
+#		self.m_solar.printResults(results)
+
 		results = self.m_solar.gatherData();
 		self.checkNames(results);
 		self.checkParameter(results, "voltage", (12.2, 13.2, 14.2, 15.2) )
@@ -201,7 +201,7 @@ class TestSolar(unittest.TestCase):
 
 
 	def test_TimestamperMock_getTime(self):
-		
+
 		self.assertEqual("2016_11_30", self.m_TimestamperMock.getDate() );
 		self.assertEqual("01:05:10", self.m_TimestamperMock.getTime() );
 
@@ -211,7 +211,7 @@ class TestSolar(unittest.TestCase):
 		self.assertEqual("00:04:09", self.m_TimestamperMock.getTime() );
 
 	def test_TimestamperMock_advanceOneSec(self):
-		
+
 		self.m_TimestamperMock.setDateTime(2015,12,31,23,59,59);
 		self.m_TimestamperMock.advanceOneSec();
 
@@ -228,30 +228,30 @@ class TestSolar(unittest.TestCase):
 		self.m_TimestamperMock.setDateTime(2016,11,30,01,05,11);
 		results = self.m_solar.gatherData();
 		self.m_solar.recordData(results);
-		
+
 		self.assertEqual("Panel", self.m_solar.m_SolarDb.m_sensorNames[0] )
 		self.assertEqual("Battery1", self.m_solar.m_SolarDb.m_sensorNames[1] )
 		self.assertEqual("Load", self.m_solar.m_SolarDb.m_sensorNames[2] )
 		self.assertEqual("Battery2", self.m_solar.m_SolarDb.m_sensorNames[3] )
-		
+
 		self.assertEqual(12.1, self.m_solar.m_SolarDb.m_voltages[0][0] )
 		self.assertEqual(13.1, self.m_solar.m_SolarDb.m_voltages[1][0] )
 		self.assertEqual(14.1, self.m_solar.m_SolarDb.m_voltages[2][0] )
 		self.assertEqual(15.1, self.m_solar.m_SolarDb.m_voltages[3][0] )
-		
+
 		self.assertEqual(500.0, self.m_solar.m_SolarDb.m_currents[0][0] )
 		self.assertEqual(400.0, self.m_solar.m_SolarDb.m_currents[1][0] )
 		self.assertEqual(300.0, self.m_solar.m_SolarDb.m_currents[2][0] )
 		self.assertEqual(200.0, self.m_solar.m_SolarDb.m_currents[3][0] )
 
-		
+
 
 
 		self.assertEqual(12.2, self.m_solar.m_SolarDb.m_voltages[0][1] )
 		self.assertEqual(13.2, self.m_solar.m_SolarDb.m_voltages[1][1] )
 		self.assertEqual(14.2, self.m_solar.m_SolarDb.m_voltages[2][1] )
 		self.assertEqual(15.2, self.m_solar.m_SolarDb.m_voltages[3][1] )
-		
+
 		self.assertEqual(600.0, self.m_solar.m_SolarDb.m_currents[0][1] )
 		self.assertEqual(500.0, self.m_solar.m_SolarDb.m_currents[1][1] )
 		self.assertEqual(400.0, self.m_solar.m_SolarDb.m_currents[2][1] )
@@ -265,17 +265,17 @@ class TestSolar(unittest.TestCase):
 		statinfo = os.stat(filename)
 		return statinfo.st_size;
 
-		
+
 	def test_SolarDb_recordData_fileWrite_notTimeToWriteYet(self):
-		
+
 		self.m_TimestamperMock.setDateTime(2016,11,30,01,0,0);
 		self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.txt"));
-		
+		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.csv"));
+
 		for index in xrange(600-1):
 			self.m_TimestamperMock.advanceOneSec();
 			self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.txt"));
+		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.csv"));
 
 		self.assertEqual("01:09:59", self.m_TimestamperMock.getTime());
 
@@ -284,38 +284,38 @@ class TestSolar(unittest.TestCase):
 		self.assertEqual("01:10:00", self.m_TimestamperMock.getTime());
 
 		self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(True, os.path.exists("test_solarLog_2016_11_30.txt"));
-		size_after_3600 = self.getFileSize("test_solarLog_2016_11_30.txt");
+		self.assertEqual(True, os.path.exists("test_solarLog_2016_11_30.csv"));
+		size_after_3600 = self.getFileSize("test_solarLog_2016_11_30.csv");
 
 		# advance to right before the next rollover. make sure no writes in between.
 		for index in xrange(600-1):
 			self.m_TimestamperMock.advanceOneSec();
 			self.m_solar.recordData( self.m_solar.gatherData() );
-			size_after_7199 = self.getFileSize("test_solarLog_2016_11_30.txt");
+			size_after_7199 = self.getFileSize("test_solarLog_2016_11_30.csv");
 			self.assertEqual(size_after_3600, size_after_7199);
 
 		self.assertEqual("01:19:59", self.m_TimestamperMock.getTime());
 
-		
+
 		# advance so that the hour rolls over.  will trigger a file write.
 		self.m_TimestamperMock.advanceOneSec();
 		self.assertEqual("01:20:00", self.m_TimestamperMock.getTime());
 
 		self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(True, os.path.exists("test_solarLog_2016_11_30.txt"));
-		size_after_7200 = self.getFileSize("test_solarLog_2016_11_30.txt");
+		self.assertEqual(True, os.path.exists("test_solarLog_2016_11_30.csv"));
+		size_after_7200 = self.getFileSize("test_solarLog_2016_11_30.csv");
 		self.assertNotEqual(size_after_7199, size_after_7200);
 
 	def test_SolarDb_recordData_fileWrite_midnight_rollover(self):
-		
+
 		self.m_TimestamperMock.setDateTime(2016,11,30,23,59,50);
 		self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.txt"));
-		
+		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.csv"));
+
 		for index in xrange(9):
 			self.m_TimestamperMock.advanceOneSec();
 			self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.txt"));
+		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_30.csv"));
 
 		self.assertEqual("23:59:59", self.m_TimestamperMock.getTime());
 
@@ -325,7 +325,7 @@ class TestSolar(unittest.TestCase):
 		self.assertEqual("2016_11_31", self.m_TimestamperMock.getDate());
 
 		self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_31.txt"));
+		self.assertEqual(False, os.path.exists("test_solarLog_2016_11_31.csv"));
 
 		# advance to right before the next rollover. make sure no writes in between.
 		for index in xrange(600-1):
@@ -334,17 +334,110 @@ class TestSolar(unittest.TestCase):
 
 		self.assertEqual("00:09:59", self.m_TimestamperMock.getTime());
 
-		
+
 		# advance so that the window rolls over.  will trigger a file write.
 		self.m_TimestamperMock.advanceOneSec();
 		self.assertEqual("00:10:00", self.m_TimestamperMock.getTime());
 
 		self.m_solar.recordData( self.m_solar.gatherData() );
-		self.assertEqual(True, os.path.exists("test_solarLog_2016_11_31.txt"));
+		self.assertEqual(True, os.path.exists("test_solarLog_2016_11_31.csv"));
+
+	def test_database_file_read(self):
+		log = self.m_solar.m_SolarDb.readDayLog("2099_99_99")
+		self.assertEqual(0, len(log));
+
+	def test_database_file_read(self):
+		log = self.m_solar.m_SolarDb.readDayLog("9999_99_99")
+		self.assertEqual(4, len(log));
+		self.assertEqual(True, ("name" in log[0]) );
+		self.assertEqual(True, ("name" in log[1]) );
+		self.assertEqual(True, ("name" in log[2]) );
+		self.assertEqual(True, ("name" in log[3]) );
+		self.assertEqual(True, ("voltage" in log[0]) );
+		self.assertEqual(True, ("voltage" in log[1]) );
+		self.assertEqual(True, ("voltage" in log[2]) );
+		self.assertEqual(True, ("voltage" in log[3]) );
+		self.assertEqual(True, ("current" in log[0]) );
+		self.assertEqual(True, ("current" in log[1]) );
+		self.assertEqual(True, ("current" in log[2]) );
+		self.assertEqual(True, ("current" in log[3]) );
+		self.assertEqual(True, ("time" in log[0]) );
+		self.assertEqual(True, ("time" in log[1]) );
+		self.assertEqual(True, ("time" in log[2]) );
+		self.assertEqual(True, ("time" in log[3]) );
+
+		self.assertEqual("Solar Panel (45)", log[0]["name"] );
+		self.assertEqual("Battery 1 (44)", log[1]["name"] );
+		self.assertEqual("Battery 2 (41)", log[2]["name"] );
+		self.assertEqual("Load (40)", log[3]["name"] );
+
+		self.assertEqual(100, len(log[0]["voltage"]) );
+		self.assertEqual(100, len(log[1]["voltage"]) );
+		self.assertEqual(100, len(log[2]["voltage"]) );
+		self.assertEqual(100, len(log[3]["voltage"]) );
+
+		self.assertEqual(100, len(log[0]["current"]) );
+		self.assertEqual(100, len(log[1]["current"]) );
+		self.assertEqual(100, len(log[2]["current"]) );
+		self.assertEqual(100, len(log[3]["current"]) );
+
+		self.assertEqual(100, len(log[0]["time"]) );
+		self.assertEqual(100, len(log[1]["time"]) );
+		self.assertEqual(100, len(log[2]["time"]) );
+		self.assertEqual(100, len(log[3]["time"]) );
 		
-		
+		# check first actual values
+		self.assertEqual(0.388, float(log[0]["voltage"][0]) );
+		self.assertEqual(12.776, float(log[1]["voltage"][0]) );
+		self.assertEqual(12.792, float(log[2]["voltage"][0]) );
+		self.assertEqual(12.756, float(log[3]["voltage"][0]) );
+
+		self.assertEqual(-1, float(log[0]["current"][0]) );
+		self.assertEqual(-14, float(log[1]["current"][0]) );
+		self.assertEqual(-129, float(log[2]["current"][0]) );
+		self.assertEqual(135, float(log[3]["current"][0]) );
+
+		self.assertEqual("20:18:19", log[0]["time"][0] );
+		self.assertEqual("20:18:19", log[1]["time"][0] );
+		self.assertEqual("20:18:19", log[2]["time"][0] );
+		self.assertEqual("20:18:19", log[3]["time"][0] );
+
+		# check second actual values
+		self.assertEqual(0.392, float(log[0]["voltage"][1]) );
+		self.assertEqual(12.776, float(log[1]["voltage"][1]) );
+		self.assertEqual(12.792, float(log[2]["voltage"][1]) );
+		self.assertEqual(12.756, float(log[3]["voltage"][1]) );
+
+		self.assertEqual(-1, int(log[0]["current"][1]) );
+		self.assertEqual(-13, int(log[1]["current"][1]) );
+		self.assertEqual(-129, int(log[2]["current"][1]) );
+		self.assertEqual(133, int(log[3]["current"][1]) );
+
+		self.assertEqual("20:18:20", log[0]["time"][1] );
+		self.assertEqual("20:18:20", log[1]["time"][1] );
+		self.assertEqual("20:18:20", log[2]["time"][1] );
+		self.assertEqual("20:18:20", log[3]["time"][1] );
+
+		# check last actual values
+		self.assertEqual(0.392, float(log[0]["voltage"][99]) );
+		self.assertEqual(12.776, float(log[1]["voltage"][99]) );
+		self.assertEqual(12.792, float(log[2]["voltage"][99]) );
+		self.assertEqual(12.757, float(log[3]["voltage"][99]) );
+
+		self.assertEqual(-1, int(log[0]["current"][99]) );
+		self.assertEqual(-13, int(log[1]["current"][99]) );
+		self.assertEqual(-129, int(log[2]["current"][99]) );
+		self.assertEqual(137, int(log[3]["current"][99]) );
+
+		self.assertEqual("20:19:59", log[0]["time"][99] );
+		self.assertEqual("20:19:59", log[1]["time"][99] );
+		self.assertEqual("20:19:59", log[2]["time"][99] );
+		self.assertEqual("20:19:59", log[3]["time"][99] );
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
-	
-	
+
+
